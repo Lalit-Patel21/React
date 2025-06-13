bf452fb7a1e7c029b52fc08a07c784c268086daa
Lalit-Patel21 <gurjarlalitpatel21@gmail.com>
2025-06-13 19:42:33 +0530

    component life cycle in class component mount didmount willunmount

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -1,31 +1,37 @@
 import React from "react";
+
 class UserClass extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      count: 0,
-      count1: 1,
+      userInfo: {
+        name: "Dummy",
+        location: "Default",
+      },
     };
-    console.log(props);
+  }
+  async componentDidMount() {
+    console.log("component did Mount");
+    // api calls
+    const data = await fetch("https://api.github.com/users/akshaymarch7");
+    const json = await data.json();
+
+    this.setState({
+      userInfo: json,
+    });
+    console.log(json);
+  }
+  componentDidUpdate() {
+    console.log("component did update");
+  }
+  componentWillUnmount() {
+    console.log("component will unMount");
   }
   render() {
-    const { name, location } = this.props;
-    const { count, count1 } = this.state;
+    const { name, location, avatar_Url } = this.state.userInfo;
     return (
       <div className="user-card">
-        <h1> Count: {count}</h1>
-        <h1> Count1: {count1}</h1>
-        <button
-          onClick={() => {
-            // never update state variable directly
-            this.setState({
-              count: this.state.count + 1,
-              count1: this.state.count1 + 1,
-            });
-          }}
-        >
-          Increase
-        </button>
+        <img src={avatar_Url} />
         <h2> Name : {name}</h2>
         <h3>Location : {location} </h3>
         <h3>Contact : lalitpatel@ </h3>
